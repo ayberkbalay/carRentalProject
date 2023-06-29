3d88e7290b02cf143aeff87b2dc3e4c84da64730
ayberkbalay <91059409+ayberkbalay@users.noreply.github.com>
2023-06-29 17:02:50 +0300

    searchManufacturer props added

--- types/index.ts
+++ types/index.ts
@@ -6,4 +6,10 @@ export interface CustomButtonProps {
     title: string;
     containerStyles?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
+    btnType?: "button" | "submit";
+}
+export interface SearchManufacturerProps {
+    manufacturer:string;
+    setManufacturer: (manufacturer:string) => void;
+
 }

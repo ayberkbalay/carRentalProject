af12fe058d9f6c23913bcbba863f397d86f98e5c
ayberkbalay <91059409+ayberkbalay@users.noreply.github.com>
2023-06-30 19:46:29 +0300

    new component ProfileBody

---
+++ components/ProfileBody.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CarCard from './CarCard'
+import { CarProps } from '@/types'
+
+const ProfileBody = () => {
+  return (
+    <div>
+x
+    </div>
+  )
+}
+
+export default ProfileBody

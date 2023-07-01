b8f1fa8570a9c24e665b3e035fa60de9507be385
ayberkbalay <91059409+ayberkbalay@users.noreply.github.com>
2023-07-01 11:42:08 +0300

    signin page changes

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,11 +1,11 @@
 import { Login } from '@components';
 import React from 'react';
 
-export default function signin({ children }: { children: React.ReactNode }) {
+export default function signin() {
     return (
         <section>
         <Login />
-        {children}
+
         </section>
     );
     }
